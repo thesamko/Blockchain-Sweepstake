3286b0c202660ce3ed92698476fca851e33dee92
thesamko <samuilkonyarov@gmail.com>
2022-06-25 10:36:44 +0100

    participants to work

--- contracts/Sweepstake.sol
+++ contracts/Sweepstake.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 //import '@openzeppelin/contracts/access/Owanble.sol';
-import '@chainlink/contracts/src/v0.6/VRFConsumerBase.sol';
+
 
 pragma solidity ^0.6.0;
 
 
-contract Sweepstake is VRFConsumerBase {
+contract Sweepstake {
     address[] public participants_entries;
     bytes32[] public teams = [bytes32("Arsenal"), "Manchester", "Liverpool"];
     Participant[] public participants;
@@ -14,19 +14,12 @@ contract Sweepstake is VRFConsumerBase {
     address public owner;
     uint public entry_fee;
     uint internal MAX_PARTICIPANTS;
-    uint public balance;
-    uint256 public fee;
-    bytes32 public keyhash;
-
-    constructor(address _VRFCoordinator, address _link, uint256 _fee, bytes32 _keyhash) public 
-    VRFConsumerBase(_VRFCoordinator, _link) {
+ 
+    constructor() public  {
         owner = msg.sender;
-        sweepstake_status = SWEEPSTAKE_STATUS.CLOSED;
+        sweepstake_status = SWEEPSTAKE_STATUS.OPEN;
         entry_fee = 10000000;
         MAX_PARTICIPANTS = 20;
-        fee = _fee;
-        keyhash = _keyhash;
-
     }
 
     struct Participant {
@@ -39,15 +32,15 @@ contract Sweepstake is VRFConsumerBase {
         sweepstake_status = SWEEPSTAKE_STATUS.OPEN;
     }
 
-    function enter() public payable {
+    function enter(uint256 _number) public payable {
         if(participants_entries.length == MAX_PARTICIPANTS) {
             setSweepstakeSoldOut();
         }
         require(sweepstake_status == SWEEPSTAKE_STATUS.OPEN,"Sweepstake is either Sold Out or Closed");
         require(msg.value >= entry_fee);
         participants_entries.push(msg.sender);
-        balance += msg.value;
-        bytes32 requestId = requestRandomness(keyhash, fee);
+        bytes32 teamRandom = getRandomTeam(_number);
+        addParticipant(msg.sender, teamRandom);
     }
 
     function setSweepstakeSoldOut() internal {
@@ -55,17 +48,10 @@ contract Sweepstake is VRFConsumerBase {
         sweepstake_status = SWEEPSTAKE_STATUS.SOLD_OUT;
     }
 
-    function getRandomTeam(uint8 _indexValue) public view returns(bytes32) {
+    function getRandomTeam(uint256 _indexValue) public view returns(bytes32) {
         return teams[_indexValue];
     }
-
-    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
-        require(_randomness > 0);
-        uint256 indexOfTeam = _randomness % teams.length;
-        bytes32 team = teams[indexOfTeam];
-        addParticipant(msg.sender, team);
-    }
-
+    
     function addParticipant(address _address, bytes32 _team) internal {
         participants.push(Participant(_address, _team));
     }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,9 @@
-from brownie import accounts, Sweepstake, config, network
+from brownie import accounts, Sweepstake
+
 account = accounts.add("0xa06ea5c253925ade7e8e4d1cd613812396e8f47f2727ca15c6aa4291b758dc9b")
+
 def sweepstake():
-    sweepstake = Sweepstake.deploy(
-        config["networks"][network.show_active()]["vrf_coordinator"],
-        config["networks"][network.show_active()]["link_token"],
-        config["networks"][network.show_active()]["fee"],
-        config["networks"][network.show_active()]["keyhash"],        
-        {"from":account})
+    sweepstake = Sweepstake.deploy({"from":account})
     return sweepstake
 
 def main():
