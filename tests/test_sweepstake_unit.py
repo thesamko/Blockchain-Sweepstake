5187b071bd1f5950bb0ab34f277cc30ca5cc5337
thesamko <samuilkonyarov@gmail.com>
2022-07-02 23:58:01 +0100

    final tests

--- tests/test_sweepstake_unit.py
+++ tests/test_sweepstake_unit.py
@@ -95,6 +95,67 @@ def test_getting_your_teams_when_soldout():
     expected_value = ['Arsenal', 'Manchester United', 'Liverpool']
     assert actual_value == expected_value
 
+def test_pay_winner_bad_actor():
+    account_owner = accounts[0]
+    account_enter = accounts[1]
+    sweepstake = Sweepstake.deploy({"from":account_owner})
+    sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    with pytest.raises(exceptions.VirtualMachineError):
+        sweepstake.payWinner(0x4c69766572706f6f6c0000000000000000000000000000000000000000000000,
+                            {"from":account_enter})
+
+def test_pay_winner():
+    account_owner = accounts[0]
+    account_enter = accounts[1]
+    sweepstake = Sweepstake.deploy({"from":account_owner})
+    tx = sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    winner_team = tx.events["PlayerTeam"]["teamRandom"]
+    account_two = accounts[2]
+    sweepstake.enter(20, {"from":account_two, "value": entry_fee})
+    sweepstake.enter(20, {"from":account_two, "value": entry_fee})
+    sweepstake_balance = sweepstake.balance()
+    starting_balance =  account_enter.balance()
+    sweepstake.payWinner(winner_team, {"from":account_owner})
+    assert sweepstake.balance() == 0
+    assert account_enter.balance() == sweepstake_balance + starting_balance
+
+def test_pay_winner_status_changed():
+    account_owner = accounts[0]
+    account_enter = accounts[1]
+    sweepstake = Sweepstake.deploy({"from":account_owner})
+    tx = sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    winner_team = tx.events["PlayerTeam"]["teamRandom"]
+    sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    sweepstake.payWinner(winner_team, {"from":account_owner})
+    assert sweepstake.sweepstake_status() == 2
+
+def test_redeploy_open_sweepstake():
+    account_owner = accounts[0]
+    account_enter = accounts[1]
+    sweepstake = Sweepstake.deploy({"from":account_owner})
+    tx = sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    winner_team = tx.events["PlayerTeam"]["teamRandom"]
+    sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    sweepstake.enter(20, {"from":account_enter, "value": entry_fee})
+    sweepstake.payWinner(winner_team, {"from":account_owner})
+    sweepstake.openSweepstake(
+        "0x417273656e616c00000000000000000000000000000000000000000000000000",
+        "0x4d616e6368657374657220556e69746564000000000000000000000000000000",
+        "0x4c69766572706f6f6c0000000000000000000000000000000000000000000000",
+        "0x4e6f7474696e6768616d00000000000000000000000000000000000000000000",
+        "0x4d616e6368657374657220436974790000000000000000000000000000000000",
+        "0x4d616e6368657374657220436974790000000000000000000000000000000000", {"from":account_owner})
+    actual_team = sweepstake.getTeam(0)
+    expected_team = "0x4e6f7474696e6768616d00000000000000000000000000000000000000000000"
+    actual_status = sweepstake.sweepstake_status()
+    assert actual_team == expected_team
+    assert actual_status == 0
+    
+
+
+
+
 
 
 
